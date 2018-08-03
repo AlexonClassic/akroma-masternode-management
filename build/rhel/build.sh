39239f7b51d3126cb62a0cf3400a9dfea210834e
Ian DesJardins <ian.d.desjardins@gmail.com>
2018-08-03 17:24:20 -0400

    Update build.sh

--- build/rhel/build.sh
+++ build/rhel/build.sh
@@ -2,8 +2,6 @@
 
 # This script starts docker and systemd on el7
 
-yum install epel-release yum install gcc python2-devel python-pip upx
-
 docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/htcondor-ce:rw  centos:centos${OS_VERSION}   /usr/sbin/init
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 docker logs $DOCKER_CONTAINER_ID
