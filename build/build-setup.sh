eef8978b5b5be24558a1f3b8b358972eaa60ec78
Ian DesJardins <ian.d.desjardins@gmail.com>
2018-08-04 01:09:15 -0400

    Update build-setup.sh

--- build/build-setup.sh
+++ build/build-setup.sh
@@ -4,6 +4,5 @@
 
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 docker logs $DOCKER_CONTAINER_ID
-docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/cgroup -v `pwd`:/akroma-masternode-management:rw  centos:centos${OS_VERSION}   /usr/sbin/init
 docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "cd /akroma-masternode-management/source && pyinstaller --onefile --noconfirm --clean --log-level=WARN --strip --runtime-tmpdir /dev/shm akroma-mn-setup.py &&
   echo -ne \"------\nEND akroma-mn-setup BUILD\n\";"
