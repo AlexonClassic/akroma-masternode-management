5da392216ffef5ed08e252651a54e4077e689093
Ian DesJardins <ian.d.desjardins@gmail.com>
2018-08-03 20:31:48 -0400

    Update build.sh

--- build/rhel/build.sh
+++ build/rhel/build.sh
@@ -7,16 +7,16 @@ docker run --privileged -d -ti -e "container=docker"  -v /sys/fs/cgroup:/sys/fs/
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
 docker logs $DOCKER_CONTAINER_ID
 echo "Line 9"
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "yum install epel-release yum install gcc python2-devel python-pip upx -y;
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "yum install epel-release yum install gcc python2-devel python-pip upx -y &&
   echo -ne \"------\nEND DEPENDENCIES INSTALL\n\";"
 echo "Line 12"
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "ls /usr/bin/pip* ; /usr/bin/pip install -r /akroma-masternode-management/source/requirements.txt;
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "find / -name *pip* ; /usr/bin/pip install -r /akroma-masternode-management/source/requirements.txt &&
   echo -ne \"------\nEND PIP REQUIREMENTS INSTALL\n\";"
 echo "Line 15"
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "pyinstaller --onefile --noconfirm --clean --log-level=WARN --strip --runtime-tmpdir /dev/shm akroma-mn-setup.py;
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "pyinstaller --onefile --noconfirm --clean --log-level=WARN --strip --runtime-tmpdir /dev/shm akroma-mn-setup.py &&
   echo -ne \"------\nEND akroma-mn-setup BUILD\n\";"
 echo "Line 18"
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "pyinstaller --onefile --noconfirm --clean --log-level=WARN --strip --runtime-tmpdir /dev/shm akroma-mn-utils.py;
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "pyinstaller --onefile --noconfirm --clean --log-level=WARN --strip --runtime-tmpdir /dev/shm akroma-mn-utils.py &&
   echo -ne \"------\nEND akroma-mn-utils BUILD\n\";"
 echo "Line 21"
 docker ps -a
