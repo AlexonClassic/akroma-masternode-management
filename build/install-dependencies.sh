86107c866d0e5be078f3e5c7c3d2d74e65d182eb
Ian DesJardins <ian.d.desjardins@gmail.com>
2018-08-04 00:51:15 -0400

    install-dependencies.sh but for centos 7.x only rn

---
+++ build/install-dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -xe
+
+# Install Dependencies within docker containers and for the building of the binaries
+
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "yum --enablerepo=extras install epel-release -y && yum --enablerepo=extras install gcc python2-devel python-pip upx -y && echo -ne \"------\nEND DEPENDENCIES INSTALL\n\";"
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "find / -name requirements.txt ; pip install -r /akroma-masternode-management/source/requirements.txt &&
+  echo -ne \"------\nEND PIP REQUIREMENTS INSTALL\n\";"
