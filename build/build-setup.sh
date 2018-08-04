bdbbe5dd1e1add53075b00ec30c5e4c1cb99bc33
Ian DesJardins <ian.d.desjardins@gmail.com>
2018-08-04 00:56:28 -0400

    Create build-setup.sh

---
+++ build/build-setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh -xe
+
+# This script builds the akroma-mn-setup binary within the docker container
+
+DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "cd /akroma-masternode-management/source && pyinstaller --onefile --noconfirm --clean --log-level=WARN --strip --runtime-tmpdir /dev/shm akroma-mn-setup.py &&
+  echo -ne \"------\nEND akroma-mn-setup BUILD\n\";"
