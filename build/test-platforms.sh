2e42a41b120c61e5facf2b353bece9e92645f122
Ian DesJardins <ian.d.desjardins@gmail.com>
2018-08-06 22:29:09 -0400

    Update test-platforms.sh

--- build/test-platforms.sh
+++ build/test-platforms.sh
@@ -3,5 +3,5 @@
 # This script will be used to test various OS types and can be extended to offer more testing to more platforms as time goes on.
 
 DOCKER_CONTAINER_ID=$(docker ps | grep centos | awk '{print $1}')
-docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "cd /akroma-masternode-management/source && ls -lah dist/akroma-mn-setup && ./akroma-mn-setup.py -h && yes | ./akroma-mn-setup.py && akroma-mn-utils
+docker exec -ti $DOCKER_CONTAINER_ID /bin/bash -xec "cd /akroma-masternode-management/source && ls -lah dist/akroma-mn-setup && ./dist/akroma-mn-setup -h && yes | ./akroma-mn-setup && akroma-mn-utils
   echo -ne \"------\nEND akroma-mn-setup AND akroma-mn-utils CENTOS 7 TEST\n\";"
